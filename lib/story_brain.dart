2f66245c5c2e3d3afc201174f06972678857ebdb
gosteve <nomuojamaro@gmail.com>
2020-05-02 14:57:21 +0100

    added visibility widget to hide the second button when at the end of the story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -34,8 +34,6 @@ class StoryBrain {
         choice2: '')
   ];
 
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
   String getStory() {
     return _storyData[_storyNumber].storyTitle;
   }
@@ -48,7 +46,6 @@ class StoryBrain {
     return _storyData[_storyNumber].choice2;
   }
 
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
   void restart() {
     _storyNumber = 0;
   }
@@ -71,7 +68,11 @@ class StoryBrain {
     }
   }
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
+  bool buttonShouldBeVisible() {
+    if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
